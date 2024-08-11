fc4bceb8bb226231dc9f6dfa23fa660829e7698a
Adrian Hernandez Islas <hernandezadri>
2024-08-11 02:14:24 -0600

    feat: config routes mercado

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['admin/dashboard'] = 'Mercado/admin';
+$route['admin/vw/mercado'] = 'Mercado/nuevo';
+$route['admin/vw/mercado/(:num)'] = 'Mercado/updateMercado/$1';
