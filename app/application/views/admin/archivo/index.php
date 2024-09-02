7ed464180db5c21c9a49c93984e009e315a12da9
Adrian Hernandez Islas <hernandezadri>
2024-09-01 18:24:18 -0600

    feat: read all images from location

--- app/application/views/admin/archivo/index.php
+++ app/application/views/admin/archivo/index.php
@@ -58,62 +58,7 @@
     <!-- JS de DataTables -->
     <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
-    <script>
-        (() => {
-
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.querySelectorAll('.needs-validation')
-
-            // Loop over them and prevent submission
-            Array.from(forms).forEach(form => {
-                form.addEventListener('submit', event => {
-                    event.preventDefault()
-                    if (!form.checkValidity()) {
-
-                        event.stopPropagation()
-                    }
-
-                    form.classList.add('was-validated')
-                }, false)
-            })
-        })();
-        const render = function(data) {
-                        return data ? `<a href="#" class="d-block mb-1 h-100">
-                                <img class="img-fluid img-thumbnail" src="https://api-files-cn1f.onrender.com/api/v1/file/${data.ruta}?width=400&height=300" alt="">
-                            </a>` : '';
-                    };
-        $("#example").DataTable({
-            ajax: {
-                url: "/api/v1/archivo/by/locacion/1",
-                dataSrc: function (json) {
-                    let result = [];
-                    for (let i = 0; i < json.length; i += 3) {
-                        result.push(json.slice(i, i + 3));
-                    }
-                    return result;
-                }
-            },
-            columns: [
-                {
-                    data: 0,
-                    render
-                },
-                {
-                    data: 1,
-                    render
-                },
-                {
-                    data: 2,
-                    render
-                }
-            ],
-            dom: 't<"bottom"ip><"clear">',
-            paging: true,
-            searching: false,
-            info: false,
-            pageLength: 2
-        });
-    </script>
+    <script src="/assets/js/feature/file/file-table.sandbox.js"></script>
 </body>
 
 </html>
--- app/assets/js/feature/file/file-table.sandbox.js
+++ app/assets/js/feature/file/file-table.sandbox.js
@@ -1,4 +1,49 @@
 const fileTableSandbox = (() => {
+  var table = null;
 
-    
+  const render = function (data) {
+    return data
+      ? `<a href="#" class="d-block mb-1 h-100">
+            <img class="img-fluid img-thumbnail" src="https://api-files-cn1f.onrender.com/api/v1/file/${data.ruta}?width=400&height=300" alt="">
+        </a>`
+      : "";
+  };
+  const init = () => {
+    table = $("#example").DataTable({
+       
+      ajax: {
+        url: "/api/v1/archivo/by/locacion/1",
+        dataSrc: function (json) {
+          let result = [];
+          for (let i = 0; i < json.length; i += 3) {
+            result.push(json.slice(i, i + 3));
+          }
+          return result;
+        },
+      },
+      columns: [
+        {
+          data: 0,
+          render,
+        },
+        {
+          data: 1,
+          render,
+        },
+        {
+          data: 2,
+          render,
+        },
+      ],
+      dom: 't<"bottom"ip><"clear">',
+      paging: true,
+      searching: false,
+      info: false,
+      pageLength: 2,
+    });
+  };
+
+  return {init}
 })();
+
+fileTableSandbox.init();
