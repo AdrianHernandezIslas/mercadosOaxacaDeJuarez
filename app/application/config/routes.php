0c3bcc912c4cc7417364f90bcf670f3629849886
AntonioLopezAurelio <antonioaure1997@gmail.com>
2024-09-02 19:45:00 -0600

    .-.

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -53,7 +53,6 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-<<<<<<< HEAD
 
 $route['admin/dashboard'] = 'Mercado/admin';
 $route['admin/vw/mercado'] = 'Mercado/nuevo';
@@ -61,6 +60,3 @@ $route['admin/vw/mercado/(:num)'] = 'Mercado/updateMercado/$1';
 
 $route['api/v1/mercado'] = 'MercadoController/create';
 $route['api/v1/mercado/(:num)'] = 'MercadoController/updateOrDelete/$1';
-=======
-//Aqui iban las rutas :v
->>>>>>> 890ef53 (Initial commit branch aure)
